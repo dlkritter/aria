f11524ae038f7b9c2fb0fe66c4142a4ad838f6a9
Enrico Granata <granata.enrico@gmail.com>
2025-12-15 15:19:59 -0700

    Make it trivial easy to add more checks to string classes

--- native-libs/unicode/src/lib.rs
+++ native-libs/unicode/src/lib.rs
@@ -11,9 +11,47 @@ use haxby_vm::{
 
 use unicode_categories::UnicodeCategories;
 
-#[derive(Default)]
-struct IsLowercaseLetter {}
-impl BuiltinFunctionImpl for IsLowercaseLetter {
+trait CharFunctionImpl {
+    fn do_check(c: char) -> bool;
+    fn name() -> &'static str;
+}
+
+macro_rules! make_char_fn {
+    ($name:ident, $method:ident) => {
+        #[allow(non_camel_case_types)]
+        #[derive(Default)]
+        struct $name {}
+
+        impl CharFunctionImpl for $name {
+            fn do_check(c: char) -> bool {
+                c.$method()
+            }
+
+            fn name() -> &'static str {
+                stringify!($name)
+            }
+        }
+    };
+}
+
+make_char_fn!(is_lowercase_letter, is_letter_lowercase);
+make_char_fn!(is_uppercase_letter, is_letter_uppercase);
+make_char_fn!(is_digit, is_number);
+make_char_fn!(is_whitespace, is_whitespace);
+
+struct CharBuiltinFunction<T: CharFunctionImpl + Default> {
+    _marker: std::marker::PhantomData<T>,
+}
+
+impl<T: CharFunctionImpl + Default> Default for CharBuiltinFunction<T> {
+    fn default() -> Self {
+        Self {
+            _marker: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<T: CharFunctionImpl + Default> BuiltinFunctionImpl for CharBuiltinFunction<T> {
     fn eval(
         &self,
         cur_frame: &mut haxby_vm::frame::Frame,
@@ -22,7 +60,7 @@ impl BuiltinFunctionImpl for IsLowercaseLetter {
         let this = VmBuiltins::extract_arg(cur_frame, |x: RuntimeValue| x.as_string().cloned())?;
         let this_raw = this.raw_value();
         if let Some(char0) = this_raw.chars().next() {
-            let is = char0.is_letter_lowercase();
+            let is = T::do_check(char0);
             cur_frame.stack.push(RuntimeValue::Boolean(is.into()));
         } else {
             todo!() // index out of bounds error
@@ -35,93 +73,10 @@ impl BuiltinFunctionImpl for IsLowercaseLetter {
     }
 
     fn name(&self) -> &str {
-        "is_lowercase_letter"
+        T::name()
     }
 }
 
-#[derive(Default)]
-struct IsUppercaseLetter {}
-impl BuiltinFunctionImpl for IsUppercaseLetter {
-    fn eval(
-        &self,
-        cur_frame: &mut haxby_vm::frame::Frame,
-        _: &mut haxby_vm::vm::VirtualMachine,
-    ) -> haxby_vm::vm::ExecutionResult<RunloopExit> {
-        let this = VmBuiltins::extract_arg(cur_frame, |x: RuntimeValue| x.as_string().cloned())?;
-        let this_raw = this.raw_value();
-        if let Some(char0) = this_raw.chars().next() {
-            let is = char0.is_letter_uppercase();
-            cur_frame.stack.push(RuntimeValue::Boolean(is.into()));
-        } else {
-            todo!() // index out of bounds error
-        }
-        Ok(RunloopExit::Ok(()))
-    }
-
-    fn arity(&self) -> haxby_vm::arity::Arity {
-        haxby_vm::arity::Arity::required(1)
-    }
-
-    fn name(&self) -> &str {
-        "is_uppercase_letter"
-    }
-}
-
-#[derive(Default)]
-struct IsDigit {}
-impl BuiltinFunctionImpl for IsDigit {
-    fn eval(
-        &self,
-        cur_frame: &mut haxby_vm::frame::Frame,
-        _: &mut haxby_vm::vm::VirtualMachine,
-    ) -> haxby_vm::vm::ExecutionResult<RunloopExit> {
-        let this = VmBuiltins::extract_arg(cur_frame, |x: RuntimeValue| x.as_string().cloned())?;
-        let this_raw = this.raw_value();
-        if let Some(char0) = this_raw.chars().next() {
-            let is = char0.is_number();
-            cur_frame.stack.push(RuntimeValue::Boolean(is.into()));
-        } else {
-            todo!() // index out of bounds error
-        }
-        Ok(RunloopExit::Ok(()))
-    }
-
-    fn arity(&self) -> haxby_vm::arity::Arity {
-        haxby_vm::arity::Arity::required(1)
-    }
-
-    fn name(&self) -> &str {
-        "is_digit"
-    }
-}
-
-#[derive(Default)]
-struct IsWhitespace {}
-impl BuiltinFunctionImpl for IsWhitespace {
-    fn eval(
-        &self,
-        cur_frame: &mut haxby_vm::frame::Frame,
-        _: &mut haxby_vm::vm::VirtualMachine,
-    ) -> haxby_vm::vm::ExecutionResult<RunloopExit> {
-        let this = VmBuiltins::extract_arg(cur_frame, |x: RuntimeValue| x.as_string().cloned())?;
-        let this_raw = this.raw_value();
-        if let Some(char0) = this_raw.chars().next() {
-            let is = char0.is_whitespace();
-            cur_frame.stack.push(RuntimeValue::Boolean(is.into()));
-        } else {
-            todo!() // index out of bounds error
-        }
-        Ok(RunloopExit::Ok(()))
-    }
-
-    fn arity(&self) -> haxby_vm::arity::Arity {
-        haxby_vm::arity::Arity::required(1)
-    }
-
-    fn name(&self) -> &str {
-        "is_whitespace"
-    }
-}
 #[unsafe(no_mangle)]
 #[allow(clippy::not_unsafe_ptr_arg_deref)]
 pub extern "C" fn dylib_haxby_inject(
@@ -135,10 +90,10 @@ pub extern "C" fn dylib_haxby_inject(
         && let Some(string) = vm.builtins.get_builtin_type_by_id(BUILTIN_TYPE_STRING)
         && let Some(string) = string.as_builtin()
     {
-        string.insert_builtin::<IsLowercaseLetter>();
-        string.insert_builtin::<IsUppercaseLetter>();
-        string.insert_builtin::<IsDigit>();
-        string.insert_builtin::<IsWhitespace>();
+        string.insert_builtin::<CharBuiltinFunction<is_lowercase_letter>>();
+        string.insert_builtin::<CharBuiltinFunction<is_uppercase_letter>>();
+        string.insert_builtin::<CharBuiltinFunction<is_digit>>();
+        string.insert_builtin::<CharBuiltinFunction<is_whitespace>>();
         return LoadResult::success();
     }
 
